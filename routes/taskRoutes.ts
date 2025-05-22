538673d57e6c3080b96c55da3ab583112791c461
LAPTOP-5J14TTMJ\ncoli <andie.hanie@gmail.com>
2025-05-22 14:19:35 +0800

    update task

--- routes/taskRoutes.ts
+++ routes/taskRoutes.ts
@@ -4,7 +4,6 @@ import { registerTask, getAllTasks } from "../controllers/taskController.ts";
 const router = Router();
 
 router.post("/register", registerTask);
-
 router.get("/", getAllTasks);
 
 export default router;
